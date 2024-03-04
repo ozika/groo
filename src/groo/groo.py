19022c5b2035e7743d1057b1817e173c37ee8c63
Ondrej Zika <zika.ondra@gmail.com>
2024-03-04 17:30:11 +0100

    v0.0.9

--- src/groo/groo.py
+++ src/groo/groo.py
@@ -5,23 +5,29 @@ def get_root(rootfile, timelim=10):
     start = time.time()
     d = Path(os.getcwd())
     found = 0   
-    while (found != 1) & ((time.time()-start) < float(timelim)):
+    while (found == 0) &  (time.time()-start)<float(3):
         if os.path.isfile(os.path.join(d, rootfile)):
-            found=1
+            found = 1
         else:
-            for root, dir, files in os.walk(d):
-                #print(root)
-                if rootfile in files:
-                    found = 1 
-                    d = root
-            if found == 0:
-                d=d.parent
-    if found == 0:
-        print("Root file not found in the time limit ("+str(timelim)+"s)\nIncrease time lim by providing timelim=XX argument")
+            d=d.parent
+
+    if found==0:
+        while (found != 1) & ((time.time()-start) < float(timelim)):
+            if os.path.isfile(os.path.join(d, rootfile)):
+                found=1
+            else:
+                for root, dir, files in os.walk(d):
+                    #print(root)
+                    if rootfile in files:
+                        found = 1 
+                        d = root
+                if found == 0:
+                    d=d.parent
+        if found == 0:
+            print("Root file not found in the time limit ("+str(timelim)+"s)\nIncrease time lim by providing timelim=XX argument")
     return d
 
 
-#print(get_root("screening_survey_neurocode.lss", timelim=5))
     
 
 
