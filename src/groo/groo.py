2f1684947b1af2171d6e31f0f8b56e79f3ac4283
Ondrej Zika <zika.ondra@gmail.com>
2024-03-06 09:22:52 +0100

    v0.1.0

--- src/groo/groo.py
+++ src/groo/groo.py
@@ -1,23 +1,33 @@
-def get_root(rootfile, timelim=10):
+def get_root(rootfile, timelim=10, verbose=False):
     import os 
     from pathlib import Path
     import time
     start = time.time()
     d = Path(os.getcwd())
     found = 0   
-    while (found == 0) &  ((time.time()-start)<float(3)):
+    # Quick search termination conditions
+    # 1/ the path has been found
+    # 2/ time limite 1s runs out 
+    # 3/ the search has root the upper-most directory
+    while (found == 0) &  (float(time.time()-start)<float(1)) & (str(d)!="/"):
+        if verbose: 
+            print("Quick search: " + str(d))
         if os.path.isfile(os.path.join(d, rootfile)):
             found = 1
         else:
             d=d.parent
 
+    d = Path(os.getcwd())
     if found==0:
+        if verbose: 
+            print("Switch to full tree search")
         while (found != 1) & ((time.time()-start) < float(timelim)):
             if os.path.isfile(os.path.join(d, rootfile)):
                 found=1
             else:
                 for root, dir, files in os.walk(d):
-                    #print(root)
+                    if verbose: 
+                        print("Full search: " + str(root))
                     if rootfile in files:
                         found = 1 
                         d = root
